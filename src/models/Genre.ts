80b83bf8f37d44bbc965f0a9c8af2b3c55e861b3
unknown <ag.prog.full@outlook.com>
2023-05-01 12:24:53 -0300

    Actualizado modelos

--- src/models/Genre.ts
+++ src/models/Genre.ts
@@ -1,51 +1,33 @@
-// import { DataType, Sequelize } from 'sequelize-typescript';
-
-// export default (sequelize: Sequelize) => {
-// sequelize.define('Genre', {
-//     id: {
-//         type: DataType.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true,
-//       },
-//      name: {
-//         type: DataType.STRING,
-//         allowNull: false,
-//       }
-//     }, { timestamps: false });
-//   };
-// import { Model, DataTypes, BelongsToManyAddAssociationMixin, BelongsToManyRemoveAssociationMixin } from 'sequelize';
-// import { Sequelize } from 'sequelize';
-// import { Videogame } from './Videogame';
-// import { sequelize } from '../db';
+import { Table, Model, Column, DataType, BelongsToMany } from "sequelize-typescript";
+import { Videogame } from "./Videogame";
 
 
-// class Genre extends Model {
-//   public id!: number;
-//   public name!: string;
 
-//   // Belongs-To-Many Association
-//   public addVideogames!: BelongsToManyAddAssociationMixin<Videogame, string>;
-//   public removeVideogames!: BelongsToManyRemoveAssociationMixin<Videogame, string>;
-// }
+@Table({
+  timestamps: false,
+  tableName: "genres",
+})
+export class Genre extends Model<Genre> {
 
-// Genre.init({
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   name: {
-//     type: DataTypes.STRING(50),
-//     allowNull: false,
-//   },
-// }, {
-//   sequelize,
-//   timestamps: false,
-//   modelName: 'Genre',
-// });
+  @Column({
+  type: DataType.INTEGER,
+  primaryKey: true,
+  autoIncrement: true,
+})
+id!: number;
+  
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name!: string;
+}
 
-// // Many-To-Many Association
-// Videogame.belongsToMany(Genre, { through: 'Videogame_Genre' });
-// Genre.belongsToMany(Videogame, { through: 'Videogame_Genre' });
+  Genre.belongsToMany(Videogame, {
+    through: "videogame_genre",
+  }); 
 
-// export { Genre };
+
+  
+  
+  
--- src/models/Videogame.ts
+++ src/models/Videogame.ts
@@ -1,130 +1,64 @@
-// import { DataType, Sequelize } from 'sequelize-typescript';
+import { Table, Model, Column, DataType, BelongsToMany } from "sequelize-typescript";
+// import { Genre } from "./Genre";
 
-// // Exportamos una función que define el modelo
-// // Luego le inyectamos la conexión a sequelize.
-// export default (sequelize: Sequelize) => {
-//   // define el modelo
-//   sequelize.define('Videogame', {
-//     id: {
-//       type: DataType.UUID,
-//       primaryKey: true,
-//       defaultValue: DataType.UUIDV4,
-//     },
-//     name: {
-//       type: DataType.STRING(50),
-//       allowNull: false,
-//     },
-//     description: {
-//       type: DataType.TEXT,
-//       allowNull: false,
-//     },
-//     released: {
-//       type: DataType.STRING(20),
-//     },
-//     rating: {
-//       type: DataType.FLOAT,
-//     },
-//     platforms: {
-//       type: DataType.ARRAY(DataType.STRING),
-//       defaultValue: [],
-//       allowNull: false,
-//     },
-//     create:{
-//       type: DataType.BOOLEAN,
-//       defaultValue: true,
-
-//     },
-//     background_image:{
-//       type: DataType.TEXT,
-
-//       allowNull: false,
-//     }
-//   }, { timestamps: false });
-// };
-
-// import { Model, DataTypes, BelongsToManyAddAssociationMixin, BelongsToManyRemoveAssociationMixin } from 'sequelize';
-// import { Sequelize } from 'sequelize';
-// import  { sequelize }  from '../db';
-// import { Genre } from './Genre';
-
-// class Videogame extends Model {
-//   public id!: string;
-//   public name!: string;
-//   public description!: string;
-//   public released!: string;
-//   public rating!: number;
-//   public platforms!: string[];
-//   public create!: boolean;
-//   public background_image!: string;
-
-//   // Belongs-To-Many Association
-//   public addGenres!: BelongsToManyAddAssociationMixin<Genre, string>;
-//   public removeGenres!: BelongsToManyRemoveAssociationMixin<Genre, string>;
-// }
-
-// Videogame.init({
-//   id: {
-//     type: DataTypes.UUID,
-//     primaryKey: true,
-//     defaultValue: DataTypes.UUIDV4,
-//   },
-//   name: {
-    //     type: DataTypes.STRING(50),
-    //     allowNull: false,
-//   },
-//   description: {
-//     type: DataTypes.TEXT,
-//     allowNull: false,
-//   },
-//   released: {
-//     type: DataTypes.STRING(20),
-//   },
-//   rating: {
-//     type: DataTypes.FLOAT,
-//   },
-//   platforms: {
-//     type: DataTypes.ARRAY(DataTypes.STRING),
-//     defaultValue: [],
-//     allowNull: false,
-//   },
-//   create:{
-//     type: DataTypes.BOOLEAN,
-//     defaultValue: true,
-//   },
-//   background_image:{
-//     type: DataTypes.TEXT,
-//     allowNull: false,
-//   }
-// }, {
-//   sequelize,
-//   timestamps: false,
-//   modelName: 'Videogame',
-// });
-
-// export { Videogame };
-
-import { Table, Model, Column, DataType } from "sequelize-typescript";
 
 @Table({
-    timestamps: false,
-    tableName: "Videogame"
+  timestamps: false,
+  tableName: "videogames",
 })
+export class Videogame extends Model<Videogame> {
 
-export class Videogame extends Model{
-    @Column({
-        type: DataType.STRING,
-        allowNull: false
-    })
-    name!: string
+  @Column({
+    type: DataType.INTEGER,
+  type: DataType.UUID,
+  primaryKey: true,
+  defaultValue: DataType.UUIDV4
+})
+id!: number;
+  
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name!: string;
 
-    @Column({
-        type: DataType.TEXT,
-        allowNull: false,
-    })
-    description!: string
+  @Column({
+    type: DataType.DATEONLY,
+  })
+  released!: Date;
 
-    @Column({
-        type: DataType.STRING(20),
+  @Column({
+    type: DataType.STRING,
+    defaultValue: 'https://thumbs.gfycat.com/DescriptiveFluidFrogmouth-size_restricted.gif',
+    allowNull: true,
+  })
+  background_image!:string
+
+  @Column({
+    type: DataType.FLOAT,
+  })
+  rating_top!: number;
+  
+  @Column({
+      type: DataType.STRING,
+      allowNull: false,
     })
-    released!: string
+    description!: string;
+  
+  
+  
+    @Column({
+      type: DataType.ARRAY(DataType.STRING),
+      defaultValue: [],
+      allowNull: false,
+    })
+    platforms!: string;
+  
+    @Column({
+      type: DataType.BOOLEAN,
+      defaultValue: true,
+    })
+    create!: string;
 }
+
+
