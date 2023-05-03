f742dee8747deae0009156e42b9cdbed00df957a
unknown <ag.prog.full@outlook.com>
2023-05-02 23:31:56 -0300

    update

--- dist/src/comparacion.js
+++ dist/src/comparacion.js
@@ -1,66 +1,43 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadApiEndpoints = void 0;
-// import GameShopData from "../../data/gameshop.json";
-const GameShopData = __importStar(require("../src/data/data.json"));
-const axios_1 = __importDefault(require("axios"));
-const loadApiEndpoints = (app) => {
-    app.get("/api", async (req, res) => {
-        try {
-            const gameShop = GameShopData;
-            if (!gameShop) {
-                return res.status(404).json({ message: "No se encontraron Juegos" });
-            }
-            return res.status(200).json(gameShop);
-        }
-        catch (error) {
-            // console.error(Error al cargar cursos: ${error});
-            console.error("No se encontraron Juegos");
-            return res.status(500).json({ message: "Error interno del servidor" });
-        }
-    });
-    app.get("/api/games", async (req, res) => {
-        try {
-            const response = await axios_1.default.get("https://api.rawg.io/api/games?page_size=10&key=20769d79ed974b54ad7a5ccd0f24c887&page_size=25");
-            const games = response.data.results.map((game) => {
-                const { id, name, background_image, rating_top } = game;
-                return { id, name, background_image, rating_top };
-            });
-            return res.status(200).json(games);
-        }
-        catch (error) {
-            // console.error(Error al cargar juegos: ${error});
-            return res.status(500).json({ message: "Error interno del servidor" });
-        }
-    });
-};
-exports.loadApiEndpoints = loadApiEndpoints;
+// import { Application, Request, Response } from "express";
+// // import GameShopData from "../../data/gameshop.json";
+//  import * as GameShopData from "../src/data/data.json";
+//  import axios from "axios";
+// interface Game {
+//   id: number;
+//   name: string;
+//   background_image: string;
+//   rating_top: number;
+// }
+// export const loadApiEndpoints = (app: Application): void => {
+//   app.get("/api", async (req: Request, res: Response) => {
+//     try {
+//       const gameShop = GameShopData;
+//       if (!gameShop) {
+//         return res.status(404).json({ message: "No se encontraron Juegos" });
+//       }
+//       return res.status(200).json(gameShop);
+//     } catch (error) {
+//     // console.error(Error al cargar cursos: ${error});
+//     console.error("No se encontraron Juegos");
+//       return res.status(500).json({ message: "Error interno del servidor" });
+//     }
+//   });
+//  app.get("/api/games", async (req: Request, res: Response) => {
+//     try {
+//       const response = await axios.get(
+//         "https://api.rawg.io/api/games?page_size=10&key=20769d79ed974b54ad7a5ccd0f24c887&page_size=25"
+//       );
+//       const games: Game[] = response.data.results.map((game: any) => {
+//         const { id, name, background_image, rating_top } = game;
+//         return { id, name, background_image, rating_top };
+//       });
+//       return res.status(200).json(games);
+//     } catch (error) {
+//       // console.error(Error al cargar juegos: ${error});
+//       return res.status(500).json({ message: "Error interno del servidor" });
+//     }
+//   });
+// };
 // export const createVideogames = async (): Promise<void> => {
 //     try {
 //       const gamesResponse: AxiosResponse<Game[]> = await axios.get<Game[]>("https://api.rawg.io/api/games?key=6df927ecdff443ffa74507df2223a6ad");
--- src/comparacion.ts
+++ src/comparacion.ts
@@ -1,49 +1,49 @@
-import { Application, Request, Response } from "express";
-// import GameShopData from "../../data/gameshop.json";
- import * as GameShopData from "../src/data/data.json";
- import axios from "axios";
+// import { Application, Request, Response } from "express";
+// // import GameShopData from "../../data/gameshop.json";
+//  import * as GameShopData from "../src/data/data.json";
+//  import axios from "axios";
 
 
-interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  rating_top: number;
-}
+// interface Game {
+//   id: number;
+//   name: string;
+//   background_image: string;
+//   rating_top: number;
+// }
 
-export const loadApiEndpoints = (app: Application): void => {
-  app.get("/api", async (req: Request, res: Response) => {
-    try {
-      const gameShop = GameShopData;
-      if (!gameShop) {
-        return res.status(404).json({ message: "No se encontraron Juegos" });
-      }
-      return res.status(200).json(gameShop);
-    } catch (error) {
-    // console.error(Error al cargar cursos: ${error});
-    console.error("No se encontraron Juegos");
-      return res.status(500).json({ message: "Error interno del servidor" });
-    }
-  });
+// export const loadApiEndpoints = (app: Application): void => {
+//   app.get("/api", async (req: Request, res: Response) => {
+//     try {
+//       const gameShop = GameShopData;
+//       if (!gameShop) {
+//         return res.status(404).json({ message: "No se encontraron Juegos" });
+//       }
+//       return res.status(200).json(gameShop);
+//     } catch (error) {
+//     // console.error(Error al cargar cursos: ${error});
+//     console.error("No se encontraron Juegos");
+//       return res.status(500).json({ message: "Error interno del servidor" });
+//     }
+//   });
 
- app.get("/api/games", async (req: Request, res: Response) => {
-    try {
-      const response = await axios.get(
-        "https://api.rawg.io/api/games?page_size=10&key=20769d79ed974b54ad7a5ccd0f24c887&page_size=25"
-      );
+//  app.get("/api/games", async (req: Request, res: Response) => {
+//     try {
+//       const response = await axios.get(
+//         "https://api.rawg.io/api/games?page_size=10&key=20769d79ed974b54ad7a5ccd0f24c887&page_size=25"
+//       );
 
-      const games: Game[] = response.data.results.map((game: any) => {
-        const { id, name, background_image, rating_top } = game;
-        return { id, name, background_image, rating_top };
-      });
+//       const games: Game[] = response.data.results.map((game: any) => {
+//         const { id, name, background_image, rating_top } = game;
+//         return { id, name, background_image, rating_top };
+//       });
 
-      return res.status(200).json(games);
-    } catch (error) {
-      // console.error(Error al cargar juegos: ${error});
-      return res.status(500).json({ message: "Error interno del servidor" });
-    }
-  });
-};
+//       return res.status(200).json(games);
+//     } catch (error) {
+//       // console.error(Error al cargar juegos: ${error});
+//       return res.status(500).json({ message: "Error interno del servidor" });
+//     }
+//   });
+// };
 
 // export const createVideogames = async (): Promise<void> => {
 //     try {
