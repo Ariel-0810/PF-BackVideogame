cbbe3f2d58aa6623c1d33146998b0383fe45d67f
unknown <ag.prog.full@outlook.com>
2023-05-02 21:28:00 -0300

    update

--- src/db.js
+++ src/db.js
@@ -9,11 +9,11 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const sequelize = new sequelize_typescript_1.Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-    logging: false,
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+// const sequelize = new sequelize_typescript_1.Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//     logging: false,
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 exports.sequelize = sequelize;
 const basename = path_1.default.basename(__filename);
 const modelDefiners = [];
